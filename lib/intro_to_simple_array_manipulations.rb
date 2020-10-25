26447ad4986e819fad39128dea0633dc55da8728
Joshua Frank <eurosewusa@gmail.com>
2020-10-25 11:19:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,17 @@
-def using_concat( a1, a2 )
-  a1.concat( a2 )
+def using_concat( array1, array2 )
+  array1.concat( array2 )
 end
+
+def using_insert( array, element )
+  array.insert( 4, element )
+end
+
+def using_uniq( array )
+  array.uniq
+end
+
+def using_flatten( array )
+  array.flatten
+end
+
+def using_delete()
